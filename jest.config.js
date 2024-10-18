06eb2492292272a43cac278dab4653f54707c48d
Aureo-Bueno <aureoalexandre@outlook.com>
2024-10-17 22:53:14 -0300

    ci: test

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverage: true,
+  coverageDirectory: '../coverage',
+  testTimeout: 30000,
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+};
